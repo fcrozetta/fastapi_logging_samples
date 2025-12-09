2bf83a49e3fbe1b94bfaed60dd107b9cc9d2913b
Fernando Crozetta <8006246+fcrozetta@users.noreply.github.com>
2025-12-09 12:24:16 +0000

    v1 working

---
+++ app/main.py
@@ -0,0 +1,72 @@
+from fastapi import FastAPI
+from prometheus_client import make_asgi_app
+from pathlib import Path
+import logging
+import os
+
+app = FastAPI(debug=True)
+
+# create auxiliary variables
+loggerName = Path(__file__).stem
+
+# create logging formatter
+fmt = os.getenv("LOG_FORMAT", " %(name)s :: %(levelname)s :: %(message)s")
+logFormatter = logging.Formatter(fmt=fmt)
+
+# create logger
+logger = logging.getLogger(loggerName)
+logger.setLevel(logging.DEBUG)
+
+# create console handler
+consoleHandler = logging.StreamHandler()
+consoleHandler.setLevel(logging.DEBUG)
+consoleHandler.setFormatter(logFormatter)
+
+# Add console handler to logger
+logger.addHandler(consoleHandler)
+
+
+# Prometheus config
+metrics_app = make_asgi_app()
+app.mount("/metrics", metrics_app)
+
+
+@app.get("/debug")
+def send_debug(message: str = "Debug message"):
+    """send a debug message"""
+    logger.debug(message)
+
+
+@app.get("/info")
+def send_info(message: str = "info message"):
+    logger.info("Sample of info")
+
+
+@app.get("/exception")
+def send_exception(message: str = "exception message"):
+    logger.exception(message)
+
+
+@app.get("/exception_stack")
+def send_exception_details(message: str = "exception_details message"):
+    logger.exception(
+        message,
+        exc_info=Exception({"message": "information inside the exception"}),
+    )
+
+
+@app.get("/error")
+def send_error(message: str = "error message"):
+    logger.error(message)
+
+
+@app.get("/critical")
+def send_critical(message: str = "critical message"):
+    logger.critical(message)
+
+
+@app.get("/stack")
+def broke_code():
+    # * this code should create an error
+    x = 1 / 0
+    return x
